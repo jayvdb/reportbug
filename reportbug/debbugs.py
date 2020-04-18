f85cf3262469c79072851d411cb185b07246ea39
Nis Martensen <nis.martensen@web.de>
2020-04-18 23:16:18 +0200

    debbugs.get_reports: remove duplicates from bug list
    Since we now run multiple queries and merge the results to get a
    comprehensive bug list, it is very likely that we end up with many
    duplicates in the bug list. Removing the duplicates allows us (further
    below) to determine the actual number of bugs as the length of the list.

--- reportbug/debbugs.py
+++ reportbug/debbugs.py
@@ -1109,6 +1109,7 @@ def get_reports(package, timeout, system='debian', mirrors=None, version=None,
                         bugs += debianbts.get_bugs(package=pkg)
                         bugs += debianbts.get_bugs(affects=pkg)
 
+        bugs = list(set(bugs))
         try:
             # retrieve bugs and generate the hierarchy
             stats = debianbts.get_status(bugs)
