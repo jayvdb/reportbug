e5b22ca33a16b4bcaa2a885ee95b6323305bca20
Nis Martensen <nis.martensen@web.de>
2020-07-05 11:26:07 +0200

    test/test_mailer.py: extend for big messages and attachments

--- test/test_mailer.py
+++ test/test_mailer.py
@@ -87,3 +87,51 @@ class TestMailtoMua(unittest.TestCase):
     def test_subject(self):
         self.assertTrue("subject=buggy-pkg%3A%20doesn%27t%20work" in self.mailto)
         self.assertTrue('work%20%20with%20continuation%20line' in self.mdict['subject'])
+
+class TestMailtoBig(unittest.TestCase):
+    def setUp(self):
+        self.mailtomua = mailer.Mailto('xdg-email')
+        self.message = email.message_from_string(message+'.'*200000)
+        self.attachments = ['/etc/hostname',
+                '/etc/dpkg/origins/default',
+                'certainly-not-existing-file',
+                ]
+        self.mailto = self.mailtomua._msg_to_mailto(self.message, self.attachments)
+        self.arglist = [x.split('=') for x in
+            self.mailto.replace('mailto:','to=').replace('?','&').split('&')]
+        self.mdict = dict(self.arglist)
+
+    def test_mailto_length(self):
+        self.assertTrue(len(self.mailto) > 128000)
+        self.assertTrue(len(self.mailto) < 130000)
+
+    def test_attachments(self):
+        # two of the specified attachments should make it to the mailto
+        # string and be specified as separate arguments
+        self.assertTrue([k for k, _ in self.arglist].count('attach') == 2)
+        self.assertEqual([v for k, v in self.arglist if k == 'attach'],
+                ['/etc/hostname', '/etc/dpkg/origins/default',])
+
+    # rest is the same as for the small message test, just to be sure
+    def test_is_cmd(self):
+        self.assertEqual(self.mailtomua.executable, "xdg-email")
+        self.assertEqual(self.mailto[0:7], "mailto:")
+
+    def test_body(self):
+        self.assertTrue("body=Package%3A%20buggy-pkg" in self.mailto)
+        self.assertTrue('%60ls%20/%60' in self.mdict['body'])
+        self.assertTrue('%24%28ls%20/usr/%29' in self.mdict['body'])
+
+    def test_to(self):
+        self.assertTrue(
+            "mailto:Debian%20Bug%20Tracking%20System%20%3Csubmit%40bugs.debian.org%3E"
+            in self.mailto
+        )
+        self.assertTrue('Alice%20%20Maintainer' in self.mdict['to'])
+
+    def test_bcc(self):
+        self.assertTrue("bcc=Debian" in self.mailto)
+
+    def test_subject(self):
+        self.assertTrue("subject=buggy-pkg%3A%20doesn%27t%20work" in self.mailto)
+        self.assertTrue('work%20%20with%20continuation%20line' in self.mdict['subject'])
