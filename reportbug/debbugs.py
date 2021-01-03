9dd2551fc298839e167686a34d4ca9c783242d9d
Nis Martensen <nis.martensen@web.de>
2021-01-03 17:11:24 +0100

    reportbug/debbugs.py: add affects for RFH, RFA, and O wnpp bugs
    Closes: #904252

--- reportbug/debbugs.py
+++ reportbug/debbugs.py
@@ -786,6 +786,8 @@ def handle_wnpp(package, bts, ui, fromaddr, timeout, online=True, http_proxy=Non
                     'The package description is:\n') % (orphstr, package)
             body = body + long_desc + '\n'
 
+        pseudos.append(f'Control: affects -1 {package}')
+
     if short_desc:
         subject = '%s: %s -- %s' % (tag, package, short_desc)
     else:
